6c04ed8b20cda9de859d94e3dd5a6fe1fc03b5fc
Madhu Balu <madhureddy.balu@gmail.com>
2025-11-17 22:59:03 +0530

    checks whether build fails or not

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p>How are you</p>
-      <p>I am fine</p>
-      <p>Checking whether it will automatically reflect on production</p>
-    </>
-  )
+  <>
+    <p>How are you</p>
+    <p>I am fine</p>
+    <p>Checking whether it will automatically reflect on production</p>
+
+    {/* Runtime error: trying to read property of undefined */}
+    {undefined.name}
+  </>
+)
+
 }
 
 export default App
